3b3c0b434eca976753e7adcf0d5a0c30333b540b
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-06-07 10:35:43 +0200

    Style episodes component

--- src/components/Detail/Episodes/Epidoses.js
+++ src/components/Detail/Episodes/Epidoses.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
+import Grid from '../../ListGrid/Grid/Grid';
+import { Typography } from '@material-ui/core';
+
+
 
 const useStyles = makeStyles(theme => ({
   root: {
-    gridColumn: ' 1 / span 2'
+    gridColumn: ' 1 / span 2',
+    boxShadow: '0px 0px 1px #000',
+    borderRadius: '5px',
+    padding: '2rem'
+  },
+  titleStyles: {
+    margin: '1rem'
   }
 
 }));
 const Episodes = (props) => {
+  const { episodes } = props;
   const classes = useStyles(props);
 
   return (
-    <div className={classes.root}>Episodesaddddffffffffffffffffffffffffffffffffff
-    afddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+    <div className={classes.root}>
+      <div className={classes.titleStyles}>
+        <Typography  variant="h5" >
+          Episodes
+        </Typography>
+      </div>
+      <Grid media={episodes}/>
     </div>
   );
 }
