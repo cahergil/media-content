d36582d35ec61e6d2415b075180b4deb08f11320
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-06-09 11:31:57 +0200

    Add prop types to Poster component

--- src/components/Detail/Poster/Poster.js
+++ src/components/Detail/Poster/Poster.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
 import ReactImageFallback from "react-image-fallback";
 
 import fallbackImage from '../../../assets/images/image_na.png';
 import classesScss from './Poster.module.scss';
-const useStyles = makeStyles(theme => ({
+
+const useStyles = makeStyles({
   root: {
     minWidth: '22.5rem',
     maxWidth: '22.5rem',
     minHeight: '20rem',
     maxHeight: '20rem',
-    // boxShadow: '0 1rem 1rem #000'
   }
-
-}));
+});
 
 const Poster = (props) => {
   const { imageUrl,title } = props;
@@ -29,4 +29,10 @@ const Poster = (props) => {
     </div>
   );
 }
+
+Poster.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
+}
+
 export default Poster;
