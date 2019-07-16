63ab56016eea1a34ca2f8f5f02a41cd8e2021daf
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-07-16 18:09:42 +0200

    Updates

---
+++ precache-manifest.52cc17f70aa85f3fb84514ce6330725d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c55e5ba912940eb7ac72d619dad6ae",
+    "url": "/media-content/index.html"
+  },
+  {
+    "revision": "29d80d0186dae315678c",
+    "url": "/media-content/static/css/main.39755996.chunk.css"
+  },
+  {
+    "revision": "3f29175809a55d85136f",
+    "url": "/media-content/static/js/2.b1174837.chunk.js"
+  },
+  {
+    "revision": "29d80d0186dae315678c",
+    "url": "/media-content/static/js/main.d7fd8c2b.chunk.js"
+  },
+  {
+    "revision": "d459d0e9ebb7c0cd2f8d",
+    "url": "/media-content/static/js/runtime~main.3bce14d7.js"
+  }
+]);
